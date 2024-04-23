3a4140b920c479838542d931b66b1029e3ddf75e
boyd-christopher-97 <j.christopher.boyd97@gmail.com>
2024-04-23 14:37:55 -0700

    All necessary files added. Connect with heroku

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+import plotly.graph_objects as go
+
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+st.header('View Data by Transmission Type')
+show_trans = st.checkbox('Not Automatic')
+if show_trans:
+    df = df[df['transmission'] != 'automatic']
+else:
+    df = df[df['transmission'] == 'automatic']
+
+st.dataframe(df)
+st.header('Vehicle types by manufacturer')
+st.write(px.histogram(df, x='manufacturer', color='type'))
+st.header('Histogram of `price` vs `condition`')
+
+# -------------------------------------------------------
+# histograms in plotly:
+# fig = go.Figure()
+# fig.add_trace(go.Histogram(x=df[df['condition']=='good']['model_year'], name='good'))
+# fig.add_trace(go.Histogram(x=df[df['condition']=='excellent']['model_year'], name='excellent'))
+# fig.update_layout(barmode='stack')
+# st.write(fig)
+# works, but too many lines of code
+# -------------------------------------------------------
+
+# histograms in plotly_express:
+st.write(px.histogram(df, x='price', color='condition'))
+# a lot more concise!
+# -------------------------------------------------------
+
+st.header('Compare price distribution between manufacturers')
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox('Select manufacturer 1',
+                              manufac_list, index=manufac_list.index('chevrolet'))
+
+manufacturer_2 = st.selectbox('Select manufacturer 2',
+                              manufac_list, index=manufac_list.index('hyundai'))
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay'))
+
+st.header('Odometer Relation to Pricing')
+scatter_plot = px.scatter(df, x="price", y="odometer",  color='manufacturer')
+st.plotly_chart(scatter_plot)
+
